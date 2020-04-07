2d2d243f877e53fcefcbea15348fa24e1db6a2c5
Ryan Webber <webber@apple.com>
2020-04-07 16:23:31 -0700

    Using bubble-up error handling

--- lua/core.lua
+++ lua/core.lua
@@ -22,35 +22,70 @@ build = {
     end
 }
 
--- A global list accumulating custom rule definitions
-local ruledefs = {}
+-- Fails without a description of where the error occurs
+-- to avoid leaking internal details
+local fatal = function(...)
+    local msg = string.format(...)
+    error(msg, 0)
+end
 
-local assertType = function(var, vtype, msg)
-    if type(var) ~= vtype then
-        error(msg)
+-- Catches any errors thrown anywere in the call hierarchy and re-throws them at the location
+-- of the callers caller. (ex. if this is used in a function a user calls, then it throws
+-- the error back at their call site)
+local assert = function(fn)
+    local _, err = pcall(fn)
+    if err then
+        error(err, 3)
     end
 end
 
-local assertString = function(var, msg) assertType(var, "string", msg) end
-local assertTable = function(var, msg) assertType(var, "table", msg) end
-local assertNumber = function(var, msg) assertType(var, "number", msg) end
+local assert_type = function(var, vtype, msg)
+    if type(var) ~= vtype then
+        fatal("%s", msg)
+    end
+end
+
+local assert_string =    function(var, msg) assert_type(var, "string", msg)   end
+local assert_table =     function(var, msg) assert_type(var, "table", msg)    end
+local assert_number =    function(var, msg) assert_type(var, "number", msg)   end
+local assert_function =  function(var, msg) assert_type(var, "function", msg) end
+
+local validate = function(var, spec)
+    for pname, pspec in pairs(spec) do
+        if pspec.required == true and var[pname] == nil then
+            fatal("Table field '%s' cannot be nil", pname, tostring(var))
+        end
+    end
+end
+
+
+-- A global list accumulating custom rule definitions
+local ruledefs = {}
 
 -- The core lua component that makes build modules spit out the build tree
 defnrule = function(name, def)
 
-    assertString(name, "Rule name must be a string")
-    assertTable(def, "Rule definition must be a table")
+    assert(function()
+        assert_string(name, "Rule name must be a string")
+        assert_table(def, "Rule definition must be a table")
+        assert_function(def.generator, "Rule definition must have a generator field")
 
-    if ruledefs[name] ~= nil then
-        error(string.format("Rule '%s' is already defined", name))
-    end
+        if ruledefs[name] ~= nil then
+            fatal("Rule '%s' is already defined", name)
+        end
+    end)
 
     ruledefs[name] = function(args)
-        local parts = def.generator(args)
-        return parts
+        assert(function()
+            assert_table(args, string.format("Arguments to rule '%s' must be a table", name))
+            if type(def.args) == "table" then validate(args, def.args) end
+        end)
+
+        return def.generator(args)
     end
 
     return ruledefs[name]
+
 end
 
 -- The most primitive rule definition just returns the inputs and outputs
@@ -71,7 +106,7 @@ setmetatable(_G, {
         elseif type(ruledefs[k]) == "function" then
             return ruledefs[k]
         else
-            error(string.format("access of undefined variable '%s'", k))
+            error(string.format("access of undefined variable '%s'", k), 2)
         end
     end
 })
