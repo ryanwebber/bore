2e6c6be60af968438765f60a3c9be0a4884c3d01
Ryan Webber <wbbr.ryan@gmail.com>
2020-04-15 10:28:15 -0700

    Better phony support for make generator

--- src/make_generator.cpp
+++ src/make_generator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 #include "make_generator.h"
 
 #define TAB "\t"
@@ -6,15 +7,30 @@
 MakeGenerator::MakeGenerator(std::filesystem::path root): root(root) {}
 
 void MakeGenerator::generate(const BuildGraph &graph) {
+
+    std::set<std::string> phonies;
+
     for (auto t : graph.getTargets()) {
         for (auto o : t->getRule()->getOutputs()) {
             generateRule(o, t->getRule()->getInputs(), t->getRule()->getCommands());
         }
 
-        // Special case for phony targets
-        if (t->getRule()->getOutputs().size() == 0) {
-            generateRule(t->getName(), t->getRule()->getInputs(), t->getRule()->getCommands());
+        // If there are no output file collisions with the target name,
+        // then add a phony target for it
+        if (graph.findTargetProducing(t->getName()) == NULL && t->getRule()->getInputs().size() > 0) {
+            std::vector<std::string> empty;
+            generateRule(t->getName(), t->getRule()->getInputs(), empty);
+            phonies.insert(t->getName());
         }
+    }
+
+    if (phonies.size() > 0) {
+        std::cerr << ".PHONY:";
+        for (auto p : phonies) {
+            std::cerr << " " << p;
+        }
+
+        std::cerr << std::endl;
     }
 }
 
