a5f38d3c2543b4848941ee5f69068ce5d82bf699
Ryan Webber <wbbr.ryan@gmail.com>
2020-04-15 08:37:12 -0700

    Implemented phony rule properly

--- pkg/core.lua
+++ pkg/core.lua
@@ -257,14 +257,29 @@ defnrule("rule", {
     end
 })
 
--- TODO: phony rule isn't really working
 defnrule("phony", {
-    generator = function(args)
-        return rule {
-            ins = {},
+    validator = function(args)
+        local all_ins = doassert(function()
+            assert_table(args.deps, "Expected phony deps to be targets")
+            local c = {}
+            for _, v in pairs(args.deps) do
+                local ins = assert_strings(v.ins, "Expected phony deps to be targets")
+                for _, input in pairs(ins) do
+                    table.insert(c, input)
+                end
+            end
+
+            return c
+        end)
+
+        return {
+            ins = all_ins,
             outs = {},
             cmds = {}
         }
+    end,
+    generator = function(args)
+        return rule(args)
     end
 })
 
